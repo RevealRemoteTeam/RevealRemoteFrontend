ea5ad61d319813afc42477651ea12a944b250c9c
Tobias Sijtsma <tobias@tobiass.nl>
2016-01-26 14:21:28 +0100

    Add StatusLed

--- www/js/app.js
+++ www/js/app.js
@@ -1,69 +1,91 @@
 angular.module('RevealRemote', ['ionic'])
 
 .controller('ControlsController', ['$scope', 'socket.io', function ($scope, socket) {
-    $scope.onTouchpadSwipe = _emitControl.bind(null);
-    $scope.toggleOverview = _emitControl.bind(null, 'toggleOverview');
-    $scope.togglePause = _emitControl.bind(null, 'togglePause');
+	$scope.onTouchpadSwipe = _emitControl.bind(null);
+	$scope.toggleOverview = _emitControl.bind(null, 'toggleOverview');
+	$scope.togglePause = _emitControl.bind(null, 'togglePause');
 
-    function _emitControl (action) {
-      socket.emit('control', {
-        action: action
-      });
-    };
+	socket.on('slide changed', function (data) {
+		$scope.$apply(function () {
+			$scope.progress = (Math.round(data.progress * 1000) / 10) + '%';
+			$scope.slideNotes = data.slideNotes;
+		});
+	});
+
+	function _emitControl (action) {
+		socket.emit('control', {
+			action: action
+		});
+	};
 }])
 
 .controller('LoginController', ['$scope', 'socket.io', function ($scope, socket) {
-  $scope.onSignInClick = function () {
-    socket.emit('handshake', {
-      nickname: this.nickname,
-      magic: this.magic
-    });
-  };
+	$scope.onSignInClick = function () {
+		socket.emit('handshake', {
+			nickname: $scope.nickname,
+			magic: $scope.magic
+		});
+	};
+}])
+
+.controller('StatusLedController', ['$scope', 'socket.io', function ($scope, socket) {
+	$scope.statusLedClass = ({ closed: 'mediocre', opening: 'connecting', open: 'good' })[socket.io.readyState];
+	socket.on('reconnecting', _setClass.bind(null, 'connecting'));
+	socket.on('reconnect_failed', _setClass.bind(null, 'bad'));
+	socket.on('connect', _setClass.bind(null, 'good'));
+	socket.on('reconnect', _setClass.bind(null, 'good'));
+
+	function _setClass (className) {
+		$scope.$apply(function () {
+			$scope.statusLedClass = className;
+		});
+	}
 }])
 
 .factory('socket.io', ['ViewSelector', function(viewSelector) {
-  var socket = io.connect('127.0.0.1:8101');
-  socket.on('ok', function () {
-    viewSelector.controls();
-  });
-  socket.on('not ok', function () {
-    // notification using toastr or smth
-    viewSelector.signIn();
-  });
-  return socket;
+	var socket = io.connect('poseidon.appisode.app.sailabove.io/presenter');
+	socket.on('ok', function () {
+		viewSelector.controls();
+	});
+	socket.on('not ok', function () {
+		// notification using toastr or smth
+		viewSelector.signIn();
+	});
+
+	return socket;
 }])
 
 .factory('ViewSelector', function () {
-  return {
-    signIn: function () {
-      document.querySelector('[data-view="sign-in"]').classList.remove('hidden');
-      document.querySelector('[data-view="controls"]').classList.add('hidden');
-    },
-    controls: function () {
-      document.querySelector('[data-view="controls"]').classList.remove('hidden');
-      document.querySelector('[data-view="sign-in"]').classList.add('hidden');
-    }
-  }
+	return {
+		signIn: function () {
+			document.querySelector('[data-view="sign-in"]').classList.remove('hidden');
+			document.querySelector('[data-view="controls"]').classList.add('hidden');
+		},
+		controls: function () {
+			document.querySelector('[data-view="controls"]').classList.remove('hidden');
+			document.querySelector('[data-view="sign-in"]').classList.add('hidden');
+		}
+	}
 })
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    !screen.lockOrientation || screen.lockOrientation('portrait');
+	$ionicPlatform.ready(function() {
+		!screen.lockOrientation || screen.lockOrientation('portrait');
 
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		if(window.cordova && window.cordova.plugins.Keyboard) {
+			// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+			// for form inputs)
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+			// Don't remove this line unless you know what you are doing. It stops the viewport
+			// from snapping when text inputs are focused. Ionic handles this internally for
+			// a much nicer keyboard experience.
+			cordova.plugins.Keyboard.disableScroll(true);
+		}
+		if(window.StatusBar) {
+			StatusBar.styleDefault();
+		}
 
 
-  });
+	});
 })
